94ed8b966d130637d97bb8688e695e8f093806ef
zhydoc2 <167150805+zhydoc2@users.noreply.github.com>
2024-07-09 18:46:58 -0500

    Update cybercity.py

--- cybercity.py
+++ cybercity.py
@@ -14,8 +14,43 @@ class Cybercity:
             'defender': 50000,
             'attacker': 50000,
         }
+        self.round = 1
+        self.rounds = 10
         self.turn = 'defender'
-        self.messages = [f"Game started. {self.turn.capitalize()}'s turn."]
+        self.messages = {
+            'defender': [],
+            'attacker': [],
+        }
+        self.messageAll(f"Game started. {self.turn.capitalize()}'s turn.")
+        self.used_locations = []  # A player cannot act on the same location twice per turn
 
-    def hackSuccessful(self, probability: float) -> bool:
+    def messageAll(self, message: str) -> None:
+        self.messages['defender'].append(message)
+        self.messages['attacker'].append(message)
+    
+    def messageEach(self, *, defender: str = '', attacker: str = '') -> None:
+        if defender:
+            self.messages['defender'].append(defender)
+        if attacker:
+            self.messages['attacker'].append(attacker)
+
+    def hack_successful(self, probability: float) -> bool:
         return random.random() < probability
+    
+    def end_turn(self) -> None:
+        self.used_locations.clear()
+        self.turn = 'attacker' if self.turn == 'defender' else 'defender'
+        
+        if self.turn == 'defender':
+            if self.round < self.rounds:
+                self.round += 1
+            else:
+                self.turn = None
+                if self.budget['attacker'] == self.budget['defender']:
+                    self.messageAll(f'Game ended in a tie.')
+                else:
+                    winner = min(self.budget)
+                    self.messageAll(f'Game ended. {winner.capitalize()} wins.')
+
+        if self.turn:
+            self.messageAll(f"{self.turn.capitalize()}'s turn.")
